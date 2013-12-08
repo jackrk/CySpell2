29c7a43dae290d5f69d022bd9bd2bac757a47122
Gamer22427 <adwirtz@iastate.edu>
2013-12-08 10:35:27 -0600

    Created MathExercises and MathFlashcards.
    Updated start.php to include math and spelling

---
+++ CySpell2/MathFlashcards.php
@@ -0,0 +1,69 @@
+<?php
+$grade = $_POST["grade"];
+$arr = array();
+$e = "";
+// Access DB
+$e = "";
+try{
+	$db = new PDO("sqlite:cyspell.db");
+}
+catch(PDOException $e){
+	$e->getMessage();
+}
+
+// Get the Problems Table ()
+$results = $db->query("SELECT * FROM math_problems");
+
+$i = 0;
+// Add the grades question-answer to arr
+foreach($results as $row){
+	$check1 = $row['grade'];
+	if($grade == $row['grade']){
+		$arr[$i] = array("picture" => $row['picture'], "answer" => $row['answer']);
+		$i++;
+	}	
+}
+
+echo <<<EOHTML
+<html>
+<head>
+<link rel="stylesheet" href="css/styles.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script src="js/jquery.transit.min.js"></script>
+<script>
+$(document).ready(function(){
+	$('.flashpic').bind('click',function(){
+		var op = $(this).css('opacity');
+		if (op > 0)	{
+			$(this).transition({opacity: 0, queue: false},150);
+			$(this).next().transition({opacity: 1, queue: false},150);
+		}
+		else {
+			$(this).transition({opacity: 1, queue: false},150);
+			$(this).next().transition({opacity: 0, queue: false},150);
+		}
+		return false;
+	});
+});
+</script>
+</head>
+<body>
+<a class="cylink" href="main.php">Home</a><br/>
+EOHTML;
+
+foreach($arr as $array){
+	$picture = $array['picture'];
+	$answer = $array['answer'];
+	echo'
+	<br/>
+	<img class="flashpic" title="Click to see the answer!!" style="cursor: pointer; z-index: 3; align: middle; margin: 0 auto;" src='; echo $picture;
+	echo' width="304" height="228">
+	<div class="flashanswer" style="cursor: pointer; color: red; opacity: 0; align: middle; margin: 0 auto; font-size: 30px; position: relative; top: -106px;">'; echo $answer; echo'</div>
+	<br/>
+	';
+}
+echo '
+</body>
+</html>
+'
+?>
--- CySpell2/start.php
+++ CySpell2/start.php
@@ -90,8 +90,42 @@ $grade = $_GET['grade'];
 			</div>
 			<div style="color: red; font-size: 30px; font-family: helvetica; top: -40px; position: relative;" >Problems </div>
 			<br>
-			<input id="submitword" style="display: block; margin: 0 auto;" type="submit" value="Start Quiz!">
+			<input id="submitword" style="display: block; margin: 0 auto;" type="submit" value="Start Spelling Quiz!">
 		</form>
-
+		<form action="MathExercises.php" method="post">
+			<input type="hidden" name="grade" value=<?php echo $grade; ?>>
+			<div id="dropdowncont">
+				<select id="probsdropdown" name="problems">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10" selected>10</option>
+					<option value="11">11</option>
+					<option value="12">12</option>
+					<option value="13">13</option>
+					<option value="14">14</option>
+					<option value="15">15</option>
+					<option value="16">16</option>
+					<option value="17">17</option>
+					<option value="18">18</option>
+					<option value="19">19</option>
+					<option value="20">20</option>
+					<option value="21">21</option>
+					<option value="22">22</option>
+					<option value="23">23</option>
+					<option value="24">24</option>
+					<option value="25">25</option>
+				</select>
+			</div>
+			<div style="color: red; font-size: 30px; font-family: helvetica; top: -40px; position: relative;" >Problems </div>
+			<br>
+			<input id="submitword" style="display: block; margin: 0 auto;" type="submit" value="Start Math Quiz!">
+		</form>
 	</body>
 </html>
